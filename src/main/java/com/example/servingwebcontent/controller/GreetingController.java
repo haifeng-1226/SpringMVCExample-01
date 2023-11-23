703e546cdf7c789ba62ff59a7ad8ee840d7e558c
yan_hf <yan_hf@neusoft.co.jp>
2023-11-23 20:10:45 +0900

    这是保存到本地的代码

--- src/main/java/com/example/servingwebcontent/controller/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/controller/GreetingController.java
@@ -11,6 +11,8 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
+		// 这里是追回的内容
+		// 追回的信息会保存到github
 		return "greeting";
 	}
 
